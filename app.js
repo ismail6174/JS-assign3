b0e1b39a3e851445eb580637638b25723ccf8352
Ismail Shah <ismailpx.6174@gmail.com>
2025-02-21 02:35:36 -0800

    Add files via upload

---
+++ app.js
@@ -0,0 +1,252 @@
+// JS Assignment 3 starts
+
+// Chapter 10 (if statements)
+
+// 1. var city = "Karachi"; if (city = "Karachi") { console.log("The City
+    // OF Lights") Correct the above statement; Also try this statement by yourself.
+
+    var city = "Karachi";
+    if (city = "Karachi") {console.log("The City Of Lights")}
+
+// 2. This is the first line of an if statement:
+// if (x === y) {
+// Complete the statement. If the condition is true, display a box
+// that asks the user value of z? and assign it to another variable.
+
+ samajh se bahir !
+
+//  3. Code an if statement that tests if ZipCode is &quot;10010&quot; so, Alert
+//  that &quot;Karachi&quot;. if not then alert (&quot;Please write correct city&quot;)
+
+var ZipCode= prompt("Enter Your City Zipcode ");
+if (ZipCode==="10010")
+    {alert("Karachi")}
+else {alert("Please write correct city")}
+
+// 4. Code an if statement. Test whether a variable has a particular
+// numerical value. If so, assign a new value to that variable, as in
+// x = 1;
+
+sir asking!
+
+
+// Chapter 11 (Comparison Operators) - Starts
+
+// 1. Code the first line of an if statement that tests whether one
+// variable is unequal to another. (Use !)
+
+var num = +prompt("Enter number");
+if (num!=6){
+    alert ("num is not equal")
+}
+else{
+    alert("num is equal")
+}
+
+// 2. Code the first line of an if statement that tests whether the
+// value represented by a variable is greater than or equal to the
+// value represented by another variable.
+
+var value=+prompt("Enter age");
+if (value>=16){
+    alert("you are young")
+}
+else{
+    alert("baray ho kr ao")
+}
+
+// 3. Code an if statement. Test whether a variable is unequal to a
+// particular number. If so, assign a number to that variable.
+
+asking sir! (nahi samajh arha)
+
+// 4. Code an if statement that tests whether a number is unequal
+// to a different number. If the condition is true (it will be),
+// display a congratulations alert.
+
+var num1=+prompt("Enter a number");
+if (num1!=7){
+    console.log("Congratulations")
+}
+
+// 5. Code a prompt asking for your first name.
+// Code an if statement that tests whether the name you entered
+// is unequal to another name.
+// If the condition is true (it will be), display an alert that says &quot;No
+// match&quot;
+
+var firstName = prompt("Enter Your first name");
+if (firstName!="jones"){
+    alert("no match")
+}
+else{
+    alert ("matched")
+}
+
+// Chapter 11 (Comparison Operators) - Ends
+
+// Chapter 12 (if…else and else if statements) - Starts
+
+// 1. Code an if statement that tests whether the value represented by
+// a variable is greater than or equal to the value represented by
+// another variable. If so, display an alert. If not, display a different
+// alert.
+
+var num1=16;
+var num2=10;
+if (num1>=num2){
+    alert("wow")
+}
+else{
+    alert("how")
+}
+
+// 2. Write a program using if else and else if statement which take
+// marks from user. And the program will calculate your percentage
+// and give you grade A/C to Your percentage. (MARKSHEET)
+
+var percent=+prompt("Enter your percent");
+if (percent>=90){
+    alert("You got A grade")
+}
+else if(percent>=70){
+    alert("You got B grade")
+}
+else if(percent>=60){
+    alert("You got C grade")
+}
+else{
+    alert("You are failed")
+}
+
+// 3. This is the if statement that begins the code.
+// if (a === 10) {
+// alert(&quot;a is 10&quot;);
+// }
+// If a isn&#39;t 10, display an alert that says The correct value of a is
+// ____
+
+var numb=+prompt("Enter number");
+if (numb===10){
+    alert("number is 10")
+}
+else if(numb!=10){
+    alert("The correct value of numb is 10")
+}
+
+// 4. Prompt the user to enter a city.
+// If the city is Karachi, display an alert acknowledging it is Karachi.
+// If not, check to see if it&#39;s Lahore.
+// If it is, display an alert acknowledging it&#39;s Lahore.
+// Otherwise, display a different alert.
+
+var city=prompt("Enter your city");
+if (city=="Karachi"){
+    alert("It is Karachi")
+}
+else if (city=="Lahore"){
+    alert("Its Lahore")
+}
+else{
+    alert("apse sheher jao")
+}
+
+// Chapter 12 (if…else and else if statements) - Ends
+
+
+
+
+// Chapter 13 (Testing sets of conditions) - Starts
+
+// 1. Code the first line of an if statement that tests whether both are
+// true: a has the same value as b and c has the same value as d.
+
+var city=prompt("Enter city");
+var age=+prompt("Enter age");
+if (city=="Karachi" && age==16){
+    alert("both condition are true")
+}
+else{
+    alert("try again")
+}
+
+// 2. Code the first line of an if statement that tests whether either or
+// both are true: a has the same value as b or c doesn&#39;t have the
+// same value as d.
+
+var name1=prompt("Enter name");
+var age=+prompt("Enter age");
+if (name1=="Ali" || age==18){
+    alert("Condition are True")
+}
+else{
+    alert("Condition is FALSE")
+}
+
+// 3. Code the first line of an if statement that tests whether I.
+// name is either Hamza or Arsalan. II. age is Less than 60.
+
+var name1 = "Hamza";
+var name2 = "Arsalan";
+var age = 45;
+
+if ((name1 === "Hamza" || name2 === "Arsalan") && age < 60) {
+    console.log("Condition is TRUE");
+} else {
+    console.log("Condition is FALSE");
+}
+
+// 4. Declare two variables and assign them number values.
+// If the first variable is less than the second variable or greater than
+// the second variable, display an alert.
+
+let num1 = 100;
+let num2 = 200;
+
+if (num1 < num2 || num1 > num2) {
+    alert("The numbers are not equal!");
+}
+
+// 5. Declare 2 variables. Assign one of them your first name and the
+// other one your last name.
+// Code 2 prompts, asking for your first and your last name.
+// If your answers match the two variables, display an alert.
+
+sir asking! (samajh nahi araha)
+
+
+// Chapter 13 (Testing sets of conditions) - Ends
+
+
+// JS Assignment 3 Ends
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
